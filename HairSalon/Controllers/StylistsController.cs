09c98c86fd6dc8b1bac1cb2664a4060a39c33952
Jase Seeley <jcseeley31590@yahoo.com>
2022-03-11 10:35:36 -0800

    add Create routes to StylistsController.

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -21,5 +21,19 @@ namespace HairSalon.Controllers
       ViewBag.PageTitle = "Stylists";
       return View(model);
     }
+
+    public ActionResult Create()
+    {
+      ViewBag.PageTitle = "Add a Stylist";
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Stylist stylist)
+    {
+      _db.Stylists.Add(stylist);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
